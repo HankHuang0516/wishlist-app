5127e9489d7c95284a81c83c66c843ddd266ec63
hankhuang0516 <hankhuang0516@gmail.com>
2026-01-19 09:29:33 +0800

    fix: Case-sensitive imports for Linux (Card, Button)

--- client/src/pages/ApiDocsPage.tsx
+++ client/src/pages/ApiDocsPage.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
-import { Card, CardContent, CardHeader, CardTitle } from '../components/ui/card';
-import { Button } from '../components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '../components/ui/Card';
+import { Button } from '../components/ui/Button';
 import { t } from '../utils/localization';
 
 interface ApiItem {

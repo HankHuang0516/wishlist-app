9d565463409e1abbc4d5c30091e07b982f98c014
Hank Huang <hankhuang0516@gmail.com>
2026-09-26 09:49:15 +0800

    Bump iOS TestFlight build to 7

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -10,7 +10,7 @@ module.exports = {
     userInterfaceStyle: 'automatic',
     ios: {
       bundleIdentifier: 'com.hankhuang.weesh',
-      buildNumber: '6',
+      buildNumber: '7',
       supportsTablet: true,
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
